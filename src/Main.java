20e355699dda83c39f514024252d7e36f0da6b0d
shubeeth001 <shubeeth001@warren.k12.in.us>
2024-08-19 13:40:52 -0400

    year

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        isLeapYear(-1600);
+        isLeapYear(1600);
+        isLeapYear(2017);
+        isLeapYear(2000);
+    }
+
+    private static boolean isLeapYear(int year) {
+        if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                if (year % 400 == 0) {
+                    return true;
+                } else {
+                    return false;
+                }
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+
+    }
+}
